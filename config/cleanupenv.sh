0c2132e8731fc279a0d7534e56efe5b0ede431b6
lsellens <lsellens@gmail.com>
2017-01-12 10:20:30 -0600

    fix clean up

---
+++ config/cleanupenv.sh
@@ -0,0 +1,12 @@
+export PATH=`echo $PATH | sed "s|$TOOLCHAIN/bin:||g"`
+unset DEVICE
+unset TOOLCHAIN
+unset ARCH
+unset CFLAGS
+unset CXXFLAGS
+unset LDFLAGS
+unset CC
+unset CXX
+unset AR
+unset RANLIB
+unset STRIP
